def324875f255fdf270c472cfeeb699b4f55951b
Lorena Ang <loreang98@hotmail.com>
2019-08-24 15:00:54 -0500

    Initial views

---
+++ RetoHack/RetoHack/Controllers/MainViews/IVAViewController.swift
@@ -0,0 +1,29 @@
+//
+//  IVAViewController.swift
+//  RetoHack
+//
+//  Created by Lore Ang on 8/24/19.
+//  Copyright © 2019 Alejandro Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class IVAViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
