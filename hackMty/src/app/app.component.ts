4895d4ba8a2e1ce0fa99c3319691eadf2ca1435e
Juan Pablo Vargas Rodríguez <jpvargas59@gmail.com>
2019-08-24 14:03:42 -0500

    Added angular project

---
+++ hackMty/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hack-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'hackMty';
+}
