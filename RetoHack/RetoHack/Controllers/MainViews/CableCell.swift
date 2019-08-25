a86f43835d0f6ac1a6034bea493fa9b45b8246e4
Lorena Ang <loreang98@hotmail.com>
2019-08-25 00:19:42 -0500

    created pie graph

---
+++ RetoHack/RetoHack/Controllers/MainViews/CableCell.swift
@@ -0,0 +1,24 @@
+//
+//  CableCell.swift
+//  RetoHack
+//
+//  Created by Lore Ang on 8/24/19.
+//  Copyright © 2019 Alejandro Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class CableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ RetoHack/RetoHack/Controllers/MainViews/CardCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  RetoHack
+//
+//  Created by Lore Ang on 8/24/19.
+//  Copyright © 2019 Alejandro Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
