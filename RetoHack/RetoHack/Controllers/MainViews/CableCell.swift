baf9f5a5741282ae6f9f07d7160591a1315b5f17
Lorena Ang <loreang98@hotmail.com>
2019-08-25 00:21:25 -0500

    created pie graph

--- RetoHack/RetoHack/Controllers/MainViews/CableCell.swift
+++ RetoHack/RetoHack/Controllers/MainViews/CableCell.swift
@@ -7,18 +7,50 @@
 //
 
 import UIKit
+import PieCharts
 
-class CableCell: UITableViewCell {
+class CableCell: UITableViewCell, PieChartDelegate {
+    
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var chartView: PieChart!
+    
+    func onSelected(slice: PieSlice, selected: Bool) {
+        print("Selected: \(selected), slice: \(slice)")
+    }
+    
+    fileprivate var currentColorIndex = 0
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    fileprivate func createModels() -> [PieSliceModel] {
+        
+        let models = [
+            PieSliceModel(value: 6, color: UIColor(red: 103/256, green: 232/256, blue: 211/256, alpha: 1)),
+            PieSliceModel(value: 4, color: UIColor(red: 4/256, green: 180/256, blue: 154/256, alpha: 1)),
+        ]
+        
+        currentColorIndex = models.count
+        return models
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    // Set up the cell
+    func configure(title: String, description: String){
+        
+        chartView.models = [
+            PieSliceModel(value: 6, color: UIColor(red: 103/256, green: 232/256, blue: 211/256, alpha: 1)),
+            PieSliceModel(value: 4, color: UIColor(red: 4/256, green: 180/256, blue: 154/256, alpha: 1)),
+        ]
+    
+        titleLabel.text = title
+        descriptionLabel.text = description
+    
+        cardView.layer.shadowColor = UIColor.gray.cgColor
+        cardView.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        cardView.layer.shadowOpacity = 1.0
+        cardView.layer.masksToBounds = false
+        cardView.layer.cornerRadius = 8.0
+        chartView.delegate = self
+        chartView.models = createModels() // order is important - models have to be set at the end
     }
-
+    
 }
--- RetoHack/RetoHack/Controllers/MainViews/CardCell.swift
+++ RetoHack/RetoHack/Controllers/MainViews/CardCell.swift
@@ -1,5 +1,5 @@
 //
-//  TableViewCell.swift
+//  CardCell.swift
 //  RetoHack
 //
 //  Created by Lore Ang on 8/24/19.
@@ -8,17 +8,8 @@
 
 import UIKit
 
-class TableViewCell: UITableViewCell {
+class CardCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
+    
 }
